51104ef3fa6b3fa38722ddf92fa74c36a1dd4d2b
James Tennant <jtennant@ucsc.edu>
2023-10-01 14:49:07 -0700

    refactor page routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import AuthForm from './auth-form';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import AccountForm from './account-form';
 
-export default function Home() {
-  return (
-    <div className="row">
-      <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
-        <p className="">
-          Experience our Auth and Storage through a simple profile management
-          example. Create a user profile and upload an avatar image. Fast,
-          simple, secure.
-        </p>
-      </div>
-      <div className="col-6 auth-widget">
-        <AuthForm />
-      </div>
-    </div>
-  );
+export default async function Account() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return <AccountForm session={session} />;
 }
