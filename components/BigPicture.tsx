2f51908b8fe3755534c175010bfe7ab01e81090f
James Tennant <jtennant@ucsc.edu>
2023-10-01 16:17:39 -0700

    autosave textarea

--- components/BigPicture.tsx
+++ components/BigPicture.tsx
@@ -1,7 +1,31 @@
+'use client';
+import { useState, useEffect, ChangeEvent, useRef } from 'react';
+
 interface Props {
   content: string;
 }
 
 export default function BigPicture({ content }: Props) {
-  return <textarea defaultValue={content} />;
+  const [text, setText] = useState(content);
+  const timerIdRef = useRef<undefined | NodeJS.Timeout>(undefined);
+  const timerActiveRef = useRef(false);
+
+  useEffect(() => {
+    clearTimeout(timerIdRef.current);
+
+    if (timerActiveRef.current) {
+      const newTimerId = setTimeout(() => {
+        console.log('save');
+      }, 2000);
+
+      timerIdRef.current = newTimerId;
+    }
+  }, [text]);
+
+  const onInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    timerActiveRef.current = true;
+  };
+
+  return <textarea value={text} onInput={onInput} />;
 }
