cdf972de32909383c02b45bf3b08c1de6e007701
James Tennant <jtennant@ucsc.edu>
2023-10-01 20:49:22 -0700

    install radix-ui

---
+++ components/Theme.tsx
@@ -0,0 +1,19 @@
+'use client';
+import '@radix-ui/themes/styles.css';
+// import '@/style/theme-config.css';
+import { ThemeProvider, useTheme } from 'next-themes';
+import { Theme as RadixTheme } from '@radix-ui/themes';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Theme({ children }: Props) {
+  return (
+    <ThemeProvider attribute="class">
+      <RadixTheme radius="small" panelBackground="solid">
+        {children}
+      </RadixTheme>
+    </ThemeProvider>
+  );
+}
