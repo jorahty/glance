d81dec4d4324619cab775ef36d6f2a4987c73936
James Tennant <jtennant@ucsc.edu>
2023-10-02 10:59:29 -0700

    rm Container

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { Flex } from '@radix-ui/themes';
 
-import Container from '@/components/Container';
 import Settings from '@/components/Settings';
 import BigPictureCard from '@/components/BigPicture/Card';
 
@@ -14,12 +13,12 @@ export default async function Account() {
   } = await supabase.from('big_pictures').select().single();
 
   return (
-    <Container>
+    <Flex p="3" gap="3" style={{ minHeight: '100vh' }}>
       <Flex direction="column" gap="3" grow="1">
         <Settings />
         <BigPictureCard initialContent={content} />
       </Flex>
       <Flex direction="column" gap="3"></Flex>
-    </Container>
+    </Flex>
   );
 }
