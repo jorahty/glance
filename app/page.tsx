41ca8f6fbd91aab3eda1c2f159f2d4b6d3c497b5
James Tennant <jtennant@ucsc.edu>
2023-10-01 20:40:23 -0700

    big picture ssr, realtime, timer

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,13 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 import BigPicture from '@/components/BigPicture';
-import AccountForm from './account-form';
 
 export default async function Account() {
   const supabase = createServerComponentClient({ cookies });
 
   const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const {
     data: { content },
   } = await supabase.from('big_pictures').select().single();
 
-  return (
-    <>
-      <AccountForm session={session} />
-      <BigPicture content={content} />
-    </>
-  );
+  return <BigPicture initialContent={content} />;
 }
--- components/BigPicture.tsx
+++ components/BigPicture.tsx
@@ -3,39 +3,51 @@ import { useState, useEffect, ChangeEvent, useRef } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 interface Props {
-  content: string;
+  initialContent: string;
 }
 
-export default function BigPicture({ content }: Props) {
+export default function BigPicture({ initialContent }: Props) {
   const supabase = createClientComponentClient();
-  const [text, setText] = useState(content);
+  const [content, setContent] = useState(initialContent);
   const timerIdRef = useRef<undefined | NodeJS.Timeout>(undefined);
-  const timerActiveRef = useRef(false);
+  const [isTyping, setIsTyping] = useState(false);
 
   useEffect(() => {
+    const channel = supabase
+      .channel('')
+      .on(
+        'postgres_changes',
+        {
+          event: '*',
+          schema: 'public',
+          table: 'big_pictures',
+        },
+        (payload: any) => {
+          if (!isTyping) setContent(payload.new.content);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase]);
+
+  const sendContent = async (content: string) => {
+    await supabase.from('big_pictures').upsert({ content });
+  };
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newContent = e.target.value;
+    setContent(newContent);
+
+    setIsTyping(true);
     clearTimeout(timerIdRef.current);
-
-    if (timerActiveRef.current) {
-      const newTimerId = setTimeout(() => {
-        updateBigPicture();
-      }, 2000);
-
-      timerIdRef.current = newTimerId;
-    }
-  }, [text]);
-
-  const onInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-    timerActiveRef.current = true;
+    timerIdRef.current = setTimeout(() => {
+      setIsTyping(false);
+      sendContent(newContent);
+    }, 800);
   };
 
-  const updateBigPicture = async () => {
-    let { error } = await supabase
-      .from('big_pictures')
-      .upsert({ content: text });
-
-    if (error) console.log(error.message);
-  };
-
-  return <textarea value={text} onInput={onInput} />;
+  return <textarea rows={100} value={content} onChange={onChange} />;
 }
