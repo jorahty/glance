a96f2335f565af72b0f828043b4fb823ca6fbf99
James Tennant <jtennant@ucsc.edu>
2023-10-02 11:26:36 -0700

    style auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Button, Flex } from '@radix-ui/themes';
 
 export default function Auth() {
   const supabase = createClientComponentClient();
@@ -13,5 +14,12 @@ export default function Auth() {
     });
   };
 
-  return <button onClick={signIn}>Sign in with Google</button>;
+  return (
+    <Flex justify="center" align="center" style={{ height: '70vh' }}>
+      <Button color="gray" variant="surface" size="4" onClick={signIn}>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+        Sign in with Google
+      </Button>
+    </Flex>
+  );
 }
