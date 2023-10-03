6ce64c4909387889ca2876c9631de4f2364ea63e
James Tennant <jtennant@ucsc.edu>
2023-10-03 14:33:31 -0700

    screen size warning

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,22 @@
 'use client';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Button, Flex } from '@radix-ui/themes';
+import { Button, Flex, Text } from '@radix-ui/themes';
+import { DesktopIcon } from '@radix-ui/react-icons';
 
 export default function Auth() {
   const supabase = createClientComponentClient();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [warningDismissed, setWarningDismissed] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const signIn = async () => {
     await supabase.auth.signInWithOAuth({
@@ -15,16 +27,35 @@ export default function Auth() {
     });
   };
 
+  const hideWarning = () => setWarningDismissed(true);
+
   return (
     <Flex justify="center" align="center" style={{ height: '70vh' }}>
-      <Button color="gray" variant="surface" size="4" onClick={signIn}>
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          alt="Google Logo"
-          width={24}
-          height={24}
-        />
-        Continue with Google
+      {windowWidth < 1000 && !warningDismissed ? (
+        <Warning dismiss={hideWarning} />
+      ) : (
+        <Button color="gray" variant="surface" size="4" onClick={signIn}>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="Google Logo"
+            width={24}
+            height={24}
+          />
+          Continue with Google
+        </Button>
+      )}
+    </Flex>
+  );
+}
+
+function Warning({ dismiss }: any) {
+  return (
+    <Flex direction="column" align="center">
+      <DesktopIcon width={24} height={24} />
+      <Text mt="4">Your screen is too narrow.</Text>
+      <Text mb="4">Use a wide-screen device.</Text>
+      <Button color="gray" variant="surface" onClick={dismiss}>
+        Continue anyway
       </Button>
     </Flex>
   );
