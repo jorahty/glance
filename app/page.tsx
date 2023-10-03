812b95f65fbb69a6d71f903ef68cf51f0ca3d3a4
James Tennant <jtennant@ucsc.edu>
2023-10-03 15:22:00 -0700

    margin-bottom

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import CourseContainer from '@/components/Course/Container';
 
 export default async function Account() {
   return (
-    <Flex p="3" gap="3" style={{ minHeight: '100vh' }}>
+    <Flex p="3" gap="3" style={{ minHeight: '100vh', marginBottom: '90vh' }}>
       <Flex direction="column" gap="3" grow="1">
         <Settings />
         <BigPictureCard />
